48496bc4380ed1cb070734e51e1fa833efa3a2ac
kellvyn1995 <kellvynstar@gmail.com>
2018-04-21 01:45:20 -0300

    questão08

---
+++ atividade-web-12-javaScript/atividade-12-questao-08.js
@@ -0,0 +1,9 @@
+function compara() {
+    var campo = Number(document.getElementById("campo").value);
+    var x =1;
+    while(x <= campo){
+        document.write(" AIA");
+        document.write( x );
+        x++;
+    }
+}
